7752e0cadb80ec8058bcf7a0699240e34e8607d8
chengjia <690676829@qq.com>
2018-10-31 14:24:54 +0800

    去掉pageoffice

--- src/main/java/com/cisdi/steel/module/test/task/DemoTask.java
+++ src/main/java/com/cisdi/steel/module/test/task/DemoTask.java
@@ -1,24 +1,24 @@
-package com.cisdi.steel.module.test.task;
-
-import com.cisdi.steel.module.test.service.DemoService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-@Component
-@Slf4j
-public class DemoTask {
-    @Autowired
-    private DemoService demoService;
-
-    @Scheduled(cron = "0 0/2 * * * ?")
-    public void synGenOfficeTast() {
-        try {
-            demoService.genOfficeFile();
-            log.info("----------------自动生成报表---------------------------");
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
-    }
-}
+//package com.cisdi.steel.module.test.task;
+//
+//import com.cisdi.steel.module.test.service.DemoService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Slf4j
+//public class DemoTask {
+//    @Autowired
+//    private DemoService demoService;
+//
+//    @Scheduled(cron = "0 0/2 * * * ?")
+//    public void synGenOfficeTast() {
+//        try {
+//            demoService.genOfficeFile();
+//            log.info("----------------自动生成报表---------------------------");
+//        } catch (Exception e) {
+//            log.error(e.getMessage());
+//        }
+//    }
+//}
