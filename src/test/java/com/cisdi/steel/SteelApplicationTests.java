9f723f67d2a91bbe2de3b1c8b4dcece0ca0aced0
落叶 <957659818@qq.com>
2018-11-14 17:13:38 +0800

    添加测试类

--- src/test/java/com/cisdi/steel/SteelApplicationTests.java
+++ src/test/java/com/cisdi/steel/SteelApplicationTests.java
@@ -1,11 +1,12 @@
 package com.cisdi.steel;
 
-import com.cisdi.steel.module.job.config.HttpProperties;
 import com.cisdi.steel.config.http.HttpUtil;
+import com.cisdi.steel.module.job.config.HttpProperties;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
