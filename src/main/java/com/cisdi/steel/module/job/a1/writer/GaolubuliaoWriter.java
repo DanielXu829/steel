84210f4c0aa47ddd6686e1cd9f24050eed575686
落叶 <957659818@qq.com>
2019-01-15 14:37:58 +0800

    高炉布料

--- src/main/java/com/cisdi/steel/module/job/a1/writer/GaolubuliaoWriter.java
+++ src/main/java/com/cisdi/steel/module/job/a1/writer/GaolubuliaoWriter.java
@@ -4,6 +4,7 @@ import cn.afterturn.easypoi.util.PoiCellUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.cisdi.steel.common.poi.PoiCustomUtil;
 import com.cisdi.steel.common.util.StringUtils;
+import com.cisdi.steel.common.util.ValidUtils;
 import com.cisdi.steel.module.job.AbstractExcelReadWriter;
 import com.cisdi.steel.module.job.dto.CellData;
 import com.cisdi.steel.module.job.dto.SheetRowCellData;
@@ -18,6 +19,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -30,6 +32,7 @@ import java.util.stream.Collectors;
  * @author leaf
  * @version 1.0
  */
+@SuppressWarnings("Duplicates")
 @Component
 public class GaolubuliaoWriter extends AbstractExcelReadWriter {
     @Override
@@ -42,16 +45,147 @@ public class GaolubuliaoWriter extends AbstractExcelReadWriter {
         // 炉料结构
         handlerPart1(workbook, dictionary, excelDTO, url);
 
+        handlerPart2(workbook, dictionary, excelDTO, url);
+
         return workbook;
     }
 
-    public static void main(String[] args) {
-        String s = "3348.0";
-        System.err.println(Double.valueOf(s).intValue());
+    private void handlerPart2(Workbook workbook, Sheet dictionary, WriterExcelDTO excelDTO, String url) {
+        int rowNum = 2;
+        // 炉料结构
+        String sheetName = PoiCellUtil.getCellValue(dictionary, rowNum, 0);
+        // 策略
+        String sheetOption = PoiCellUtil.getCellValue(dictionary, rowNum, 1);
+        // 点名
+        String tagName = PoiCellUtil.getCellValue(dictionary, rowNum, 2);
+        // 最后一个值
+        String lastVal = PoiCellUtil.getCellValue(dictionary, rowNum, 3);
+        // 写入数据的行数
+        String rowIndexStr = PoiCellUtil.getCellValue(dictionary, rowNum, 4);
+        Integer rowIndex = 3;
+        if (StringUtils.isNotBlank(rowIndexStr)) {
+            rowIndex = Double.valueOf(rowIndexStr).intValue();
+        }
+        Sheet sheet = workbook.getSheet(sheetName);
+        // 最后一个值
+        String lastValKey = null;
+        if (StringUtils.isNotBlank(lastVal)) {
+            lastValKey = lastVal;
+        }
+        // 指定行的值
+        List<String> rowVals = PoiCustomUtil.getRowCelVal(sheet, 2);
+        List<CellData> rowCellDataList = new ArrayList<>();
+        // 处理后的时间
+        List<DateQuery> dateQueries = handlerDate(sheetOption, excelDTO.getDateQuery().getRecordDate());
+        for (DateQuery dateQuery : dateQueries) {
+            List<String> indexData = eachData(url, dateQuery, tagName);
+            if (Objects.nonNull(indexData)) {
+                List<Integer> indexs = indexData.stream().map(v -> Integer.parseInt(v.split("/")[0])).collect(Collectors.toList());
+                List<String> times = indexData.stream().map(v -> v.split("/")[1]).collect(Collectors.toList());
+                int min = 0;
+                if (Objects.nonNull(lastValKey) && indexData.contains(lastValKey)) {
+                    min = indexData.indexOf(lastValKey) + 1;
+                }
+                int max = indexData.size();
+
+                for (int i = min; i < max; i++) {
+                    Integer chargeNo = indexs.get(i);
+                    Integer newRowIndex = rowIndex + i * 4;
+                    List<CellData> cellDataList = changeLiaozhiData(url, chargeNo.toString(), times.get(i), newRowIndex, rowVals);
+                    rowCellDataList.addAll(cellDataList);
+                }
+                if (max > 0) {
+                    updateDictChargeNo(dictionary, indexData.get(max - 1), rowNum, 3);
+                    updateDictChargeNo(dictionary, max*4 + rowIndex, rowNum, 4);
+                }
+
+            }
+        }
+        SheetRowCellData build = SheetRowCellData.builder()
+                .workbook(workbook)
+                .sheet(sheet)
+                .cellDataList(rowCellDataList)
+                .build();
+        build.allValueWriteExcel();
     }
 
-    private void handlerPart1(Workbook workbook, Sheet dictionary, WriterExcelDTO excelDTO, String url) {
+    private List<CellData> changeLiaozhiData(String url, String chargeNo, String time, int rowIndex, List<String> rowVals) {
+        url += "/charge/variation?chargeNo=" + chargeNo;
+        String s = httpUtil.get(url);
+        List<CellData> resultData = new ArrayList<>();
+        if (StringUtils.isBlank(s)) {
+            return resultData;
+        }
+        JSONObject object = JSONObject.parseObject(s);
+        JSONObject data = object.getJSONObject("data");
+        if (Objects.isNull(data)) {
+            return resultData;
+        }
+        Object chargeIndexInDay = data.get("chargeIndexInDay");
+        JSONObject materialValues = data.getJSONObject("materialValues");
+        if (Objects.isNull(materialValues)) {
+            return resultData;
+        }
+        materialValues.put("chargeIndexInDay", chargeIndexInDay);
+        materialValues.put("chargeNo", chargeNo);
+        materialValues.put("time", new Date(Long.parseLong(time)));
+        Object oreStockline = materialValues.get("oreStockline");
+        if (Objects.nonNull(oreStockline)) {
+            BigDecimal multiply = new BigDecimal(oreStockline.toString()).multiply(new BigDecimal(1000));
+            materialValues.put("oreStockline", multiply);
+        }
+        int size = rowVals.size();
+        JSONObject distribution = data.getJSONObject("distribution");
 
+        for (int i = 0; i < size; i++) {
+            String s1 = rowVals.get(i);
+            if (StringUtils.isBlank(s1)) {
+                continue;
+            }
+            boolean intege = ValidUtils.isDecmal(s1);
+            if (!intege) {
+                Object obj = materialValues.get(s1);
+                ExcelWriterUtil.addCellData(resultData, rowIndex, i, obj);
+                continue;
+            }
+            if (Double.valueOf(s1).intValue() == 1) {
+                ExcelWriterUtil.addCellData(resultData, rowIndex, i - 1, "PWC");
+                ExcelWriterUtil.addCellData(resultData, rowIndex + 2, i - 1, "PWO");
+            }
+            handlerDistribution(distribution, "C", Double.valueOf(s1).intValue() + "", rowIndex, i, resultData);
+            handlerDistribution(distribution, "O", Double.valueOf(s1).intValue() + "", rowIndex + 2, i, resultData);
+
+        }
+        return resultData;
+
+    }
+
+    private void handlerDistribution(JSONObject distribution,
+                                     String type,
+                                     String key,
+                                     Integer rowIndex,
+                                     Integer columnIndex,
+                                     List<CellData> resultData
+    ) {
+        JSONObject obj = distribution.getJSONObject(type);
+        JSONObject jsonObject = obj.getJSONObject(key);
+        if (Objects.isNull(jsonObject)) {
+            return;
+        }
+        BigDecimal degreeSet = jsonObject.getBigDecimal("degreeSet");
+        BigDecimal roundSet = jsonObject.getBigDecimal("roundSet");
+        if (Objects.isNull(degreeSet) || Objects.isNull(roundSet)) {
+            return;
+        }
+        if (BigDecimal.ZERO.doubleValue() == degreeSet.doubleValue() || BigDecimal.ZERO.doubleValue() == roundSet.doubleValue()) {
+            return;
+        }
+        ExcelWriterUtil.addCellData(resultData, rowIndex, columnIndex, degreeSet);
+        ExcelWriterUtil.addCellData(resultData, rowIndex + 1, columnIndex, roundSet);
+    }
+
+
+    private void handlerPart1(Workbook workbook, Sheet dictionary, WriterExcelDTO excelDTO, String url) {
         // 炉料结构
         String sheetName = PoiCellUtil.getCellValue(dictionary, 1, 0);
         // 策略
@@ -81,17 +215,13 @@ public class GaolubuliaoWriter extends AbstractExcelReadWriter {
         for (DateQuery dateQuery : dateQueries) {
             List<String> indexData = eachData(url, dateQuery, tagName);
             if (Objects.nonNull(indexData)) {
-
                 List<Integer> indexs = indexData.stream().map(v -> Integer.parseInt(v.split("/")[0])).collect(Collectors.toList());
                 List<String> times = indexData.stream().map(v -> v.split("/")[1]).collect(Collectors.toList());
-
-
                 int min = 0;
                 if (Objects.nonNull(lastValKey) && indexData.contains(lastValKey)) {
                     min = indexData.indexOf(lastValKey) + 1;
                 }
                 int max = indexs.size();
-
                 for (int i = min; i < max; i++) {
                     Integer chargeNo = indexs.get(i);
                     List<CellData> cellDataList = changeData(url, chargeNo.toString(), times.get(i), rowIndex + i, rowVals);
