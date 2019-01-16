482c0ebfcb68c954121191b411d22d2bfda4bcdc
chengjia <690676829@qq.com>
2019-01-16 09:00:58 +0800

    Merge branch 'master' of https://gitee.com/ypfor/steel
    # Conflicts:
    #	src/test/java/com/cisdi/steel/job/a1/TestJob1.java

---
+++ src/main/java/com/cisdi/steel/module/job/a1/execute/GaoLuPenMeiExecute.java
@@ -0,0 +1,31 @@
+package com.cisdi.steel.module.job.a1.execute;
+
+import com.cisdi.steel.module.job.AbstractJobExecuteExecute;
+import com.cisdi.steel.module.job.IExcelReadWriter;
+import com.cisdi.steel.module.job.a1.writer.GaoLuPenMeiWriter;
+import com.cisdi.steel.module.job.a1.writer.LudingbuliaoWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 高炉喷煤
+ * <p>Description:         </p>
+ * <p>email: ypasdf@163.com</p>
+ * <p>Copyright: Copyright (c) 2018</p>
+ * <P>Date: 2018/11/13 </P>
+ *
+ * @author leaf
+ * @version 1.0
+ */
+@Component
+public class GaoLuPenMeiExecute extends AbstractJobExecuteExecute {
+
+
+    @Autowired
+    private GaoLuPenMeiWriter gaoLuPenMeiWriter;
+
+    @Override
+    public IExcelReadWriter getCurrentExcelWriter() {
+        return gaoLuPenMeiWriter;
+    }
+}
