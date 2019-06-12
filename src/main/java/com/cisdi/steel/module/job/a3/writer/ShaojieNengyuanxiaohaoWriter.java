fbc7b63c96987c704f6a0c4512723f42292aacec
chengjia <690676829@qq.com>
2019-06-12 10:16:27 +0800

    烧结公辅能源消耗及成本统计表

--- src/main/java/com/cisdi/steel/module/job/a3/writer/ShaojieNengyuanxiaohaoWriter.java
+++ src/main/java/com/cisdi/steel/module/job/a3/writer/ShaojieNengyuanxiaohaoWriter.java
@@ -17,10 +17,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * 烧结能源消耗及成本统计
@@ -63,86 +60,129 @@ public class ShaojieNengyuanxiaohaoWriter extends AbstractExcelReadWriter {
                     version = "6.0";
                 }
                 List<DateQuery> dateQueries = this.getHandlerData(sheetSplit, date.getRecordDate());
-                for (DateQuery dateQuery : dateQueries) {
-                    //请求参数
-                    JSONObject query = new JSONObject();
-                    int rowBatch = 3;
-                    //获取对应的请求接口地址
-                    String url = getUrl4(version);
+                String url = "";
+                if ("_5tag_month_all".equals(sheetName) || "_6tag_month_all".equals(sheetName)) {
+                    url = getUrl4(version);
+                    for (DateQuery dateQuery : dateQueries) {
+                        int rowBatch = 1;
+                        //获取对应的请求接口地址
+                        Map query = new HashMap();
+                        query.put("startTime", dateQuery.getQueryStartTime());
+                        query.put("endTime", dateQuery.getQueryEndTime());
 
-                    JSONObject sortMap = new JSONObject();
-                    sortMap.put("recordDate", "ASC");
+                        List<String> columns = PoiCustomUtil.getFirstRowCelVal(sheet);
+                        List<CellData> cellDataList = dealByTime(url, date.getRecordDate(), columns, query, rowBatch);
+                        ExcelWriterUtil.setCellValue(sheet, cellDataList);
+                    }
+                } else if ("_5tag2_month_all".equals(sheetName) || "_6tag2_month_all".equals(sheetName)) {
+                    url = getUrl5(version);
+                    for (DateQuery dateQuery : dateQueries) {
+                        List<String> columns = PoiCustomUtil.getFirstRowCelVal(sheet);
+                        int rowBatch = 1;
+                        //获取对应的请求接口地址
+                        JSONObject query = new JSONObject();
+                        query.put("start", dateQuery.getQueryStartTime());
+                        query.put("end", dateQuery.getQueryEndTime());
+                        query.put("tagNames", columns);
 
-                    List<JSONObject> clauses = new ArrayList<>();
-                    dealClauses(clauses, "recordDate", ">=", DateUtil.getFormatDateTime(dateQuery.getStartTime(), DateUtil.fullFormat));
-                    dealClauses(clauses, "recordDate", "<=", DateUtil.getFormatDateTime(dateQuery.getEndTime(), DateUtil.fullFormat));
-
-                    query.put("clauses", clauses);
-                    query.put("sortMap", sortMap);
-
-                    List<String> columns = PoiCustomUtil.getFirstRowCelVal(sheet);
-                    List<CellData> cellDataList = mapDataHandler(url, query, columns, date.getRecordDate(), rowBatch);
-                    ExcelWriterUtil.setCellValue(sheet, cellDataList);
+                        SerializeConfig serializeConfig = new SerializeConfig();
+                        String jsonString = JSONObject.toJSONString(query, serializeConfig);
+                        String result = httpUtil.postJsonParams(url, jsonString);
+                        if (StringUtils.isBlank(result)) {
+                            return null;
+                        }
+                        JSONObject jsonObject = JSONObject.parseObject(result);
+                        JSONObject data = jsonObject.getJSONObject("data");
+                        if (Objects.isNull(data)) {
+                            return null;
+                        }
+                        List<CellData> cellDataList = handlerJsonArray(columns, data, rowBatch, dateQuery);
+                        ExcelWriterUtil.setCellValue(sheet, cellDataList);
+                    }
                 }
+
             }
         }
         return workbook;
     }
 
-    private void dealClauses(List<JSONObject> clauses, String column, String operation, String value) {
-        JSONObject clause = new JSONObject();
-        clause.put("column", column);
-        clause.put("operation", operation);
-        clause.put("value", value);
-        clauses.add(clause);
-    }
-
-    private List<CellData> mapDataHandler(String url, JSONObject query, List<String> columns, Date date, int rowBatch) {
-
+    private List<CellData> handlerJsonArray(List<String> columns, JSONObject data, int rowBatch, DateQuery dateQuery) {
         List<CellData> cellDataList = new ArrayList<>();
-        SerializeConfig serializeConfig = new SerializeConfig();
-        String jsonString = JSONObject.toJSONString(query, serializeConfig);
-        String result = httpUtil.postJsonParams(url, jsonString);
-        if (StringUtils.isNotBlank(result)) {
-            JSONObject jsonObject = JSONObject.parseObject(result);
-            if (Objects.nonNull(jsonObject)) {
-                dealByTime(jsonObject, date, columns, cellDataList, rowBatch);
+        int size = columns.size();
+        for (int i = 0; i < size; i++) {
+            String column = columns.get(i);
+            int rowIndex = 1;
+            if (StringUtils.isNotBlank(column)) {
+                JSONObject jsonObject = data.getJSONObject(column);
+                if (Objects.nonNull(jsonObject)) {
+                    Map<String, Object> innerMap = jsonObject.getInnerMap();
+                    Set<String> keys = innerMap.keySet();
+                    Long[] list = new Long[keys.size()];
+                    int k = 0;
+                    for (String key : keys) {
+                        list[k] = Long.valueOf(key);
+                        k++;
+                    }
+                    Arrays.sort(list);
+
+                    List<DateQuery> dateQueries = DateQueryUtil.buildMonthDayEach(dateQuery.getRecordDate());
+
+                    for (int j = 0; j < dateQueries.size(); j++) {
+                        long time = dateQueries.get(j).getStartTime().getTime();
+                        Object v = "";
+                        for (int m = 0; m < list.length; m++) {
+                            if (time == list[m].longValue()) {
+                                Object o = innerMap.get(String.valueOf(list[m]));
+                                v = o;
+                                break;
+                            }
+                        }
+                        ExcelWriterUtil.addCellData(cellDataList, rowIndex, i, v);
+                        rowIndex += rowBatch;
+                    }
+                }
             }
         }
-
         return cellDataList;
     }
 
-
-    private void dealByTime(JSONObject jsonObject, Date date, List<String> columns, List<CellData> cellDataList, int rowbatch) {
-        JSONArray rows = jsonObject.getJSONArray("rows");
-        if (Objects.nonNull(rows) && rows.size() > 0) {
-            List<DateQuery> monthDayEach = DateQueryUtil.buildMonthDayEach(date);
-            int rowIndex = 1;
-            for (int j = 0; j < monthDayEach.size(); j++) {
-                DateQuery dateQuery = monthDayEach.get(j);
-                int r = rowIndex;
-                for (int i = 0; i < rows.size(); i++) {
-                    JSONObject jsonObject1 = rows.getJSONObject(i);
-                    if (Objects.nonNull(jsonObject1)) {
-                        String recordDate = jsonObject1.getString("recordDate");
-                        Date strToDate = DateUtil.strToDate(recordDate, DateUtil.fullFormat);
-                        String formatDateTime = DateUtil.getFormatDateTime(strToDate, "yyyy-MM-dd");
-                        if (formatDateTime.equals(DateUtil.getFormatDateTime(dateQuery.getRecordDate(), "yyyy-MM-dd"))) {
-                            for (int m = 0; m < columns.size(); m++) {
-                                String s = columns.get(m);
-                                if (StringUtils.isNotBlank(s)) {
-                                    Object o = jsonObject1.get(s);
-                                    ExcelWriterUtil.addCellData(cellDataList, r, m, o);
+    private List<CellData> dealByTime(String url, Date date, List<String> columns, Map query, int rowbatch) {
+        List<CellData> cellDataList = new ArrayList<>();
+        String result = httpUtil.get(url, query);
+        if (StringUtils.isNotBlank(result)) {
+            JSONObject jsonObject = JSONObject.parseObject(result);
+            if (Objects.nonNull(jsonObject)) {
+                JSONArray rows = jsonObject.getJSONArray("data");
+                if (Objects.nonNull(rows) && rows.size() > 0) {
+                    List<DateQuery> monthDayEach = DateQueryUtil.buildMonthDayEach(date);
+                    int rowIndex = 1;
+                    for (int j = 0; j < monthDayEach.size(); j++) {
+                        DateQuery dateQuery = monthDayEach.get(j);
+                        int r = rowIndex;
+                        for (int i = 0; i < rows.size(); i++) {
+                            JSONObject jsonObject1 = rows.getJSONObject(i);
+                            if (Objects.nonNull(jsonObject1)) {
+                                String recordDate = jsonObject1.getString("recordDate");
+                                Date strToDate = DateUtil.strToDate(recordDate, DateUtil.fullFormat);
+                                String formatDateTime = DateUtil.getFormatDateTime(strToDate, "yyyy-MM-dd");
+                                if (formatDateTime.equals(DateUtil.getFormatDateTime(dateQuery.getRecordDate(), "yyyy-MM-dd"))) {
+                                    for (int m = 0; m < columns.size(); m++) {
+                                        String s = columns.get(m);
+                                        if (StringUtils.isNotBlank(s)) {
+                                            Object o = jsonObject1.get(s);
+                                            ExcelWriterUtil.addCellData(cellDataList, r, m, o);
+                                        }
+                                    }
+                                    r++;
                                 }
                             }
-                            r++;
                         }
+                        rowIndex += rowbatch;
                     }
                 }
-                rowIndex += rowbatch;
             }
         }
+        return cellDataList;
     }
 
 
@@ -155,12 +195,27 @@ public class ShaojieNengyuanxiaohaoWriter extends AbstractExcelReadWriter {
     //无纸化-脱硫运行记录
     private String getUrl4(String version) {
         if ("5.0".equals(version)) {
-            return httpProperties.getUrlApiSJOne() + "/ploDesulfuration/select";
+            return httpProperties.getUrlApiSJOne() + "/ploDesulfuration/statistics";
         } else {
             // "6.0".equals(version) 默认
-            return httpProperties.getUrlApiSJTwo() + "/ploDesulfuration/select";
+            return httpProperties.getUrlApiSJTwo() + "/ploDesulfuration/statistics";
         }
     }
 
+
+    /**
+     * 不同的版本获取不同的接口地址
+     *
+     * @param version 版本号
+     * @return 结果
+     */
+    private String getUrl5(String version) {
+        if ("5.0".equals(version)) {
+            return httpProperties.getUrlApiSJOne() + "/tagValues/tagNames";
+        } else {
+            // "6.0".equals(version) 默认
+            return httpProperties.getUrlApiSJTwo() + "/tagValues/tagNames";
+        }
+    }
 
 }
