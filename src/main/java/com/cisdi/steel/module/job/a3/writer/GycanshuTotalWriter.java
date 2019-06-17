56e30d05c0681be51b6f9012770d8c0beaa76959
chengjia <690676829@qq.com>
2019-06-17 09:09:51 +0800

    烧结修改

--- src/main/java/com/cisdi/steel/module/job/a3/writer/GycanshuTotalWriter.java
+++ src/main/java/com/cisdi/steel/module/job/a3/writer/GycanshuTotalWriter.java
@@ -43,7 +43,7 @@ public class GycanshuTotalWriter extends AbstractExcelReadWriter {
             String[] sheetSplit = sheetName.split("_");
             int picTime = -1;
             int currDateTime = Integer.valueOf(DateUtil.getFormatDateTime(DateUtil.addHours(date.getRecordDate(), -1), "HH"));
-            picTime = currDateTime;
+            picTime = Integer.valueOf(DateUtil.getFormatDateTime(date.getRecordDate(), "HH"));
             if (sheetSplit.length == 4) {
                 String main1 = sheetSplit[1];
                 String lastExt = sheetSplit[3];
