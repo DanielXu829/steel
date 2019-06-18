fe2ee165510dabb2221e3d98bfb3e2e0e6548840
yjq123 <593064550@qq.com>
2019-06-18 16:51:44 +0800

    删除CK12自动配煤

--- src/main/java/com/cisdi/steel/module/job/enums/JobEnum.java
+++ src/main/java/com/cisdi/steel/module/job/enums/JobEnum.java
@@ -73,7 +73,6 @@ public enum JobEnum {
 
     // 焦化
     jh_zidongpeimei("jh_zidongpeimei", "配煤-自动配煤报表（班）"),
-    jh_ck12zidongpeimei("jh_ck12zidongpeimei", "CK12-配煤-自动配煤报表（班）"),
     jh_ck12zidongpeimeinew("jh_ck12zidongpeimeinew", "CK12-配煤-自动配煤报表（班）-New"),
     jh_ck45zidongpeimei("jh_ck45zidongpeimei", "CK45-配煤-自动配煤报表"),
     jh_fensuixidu("jh_fensuixidu", "配煤-粉碎细度报表（月）"),
