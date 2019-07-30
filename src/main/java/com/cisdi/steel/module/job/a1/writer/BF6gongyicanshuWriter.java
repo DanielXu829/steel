134d8a59178a38791d30328181ae53584d388152
suntao <s6511@163.com>
2019-07-30 09:03:57 +0800

    不在继承GuankongzhibiaoWriter，直接继承AbstractExcelReadWriter

--- src/main/java/com/cisdi/steel/module/job/a1/writer/BF6gongyicanshuWriter.java
+++ src/main/java/com/cisdi/steel/module/job/a1/writer/BF6gongyicanshuWriter.java
@@ -1,14 +1,22 @@
 package com.cisdi.steel.module.job.a1.writer;
 
+import com.alibaba.fastjson.JSONObject;
 import com.cisdi.steel.common.poi.PoiCustomUtil;
 import com.cisdi.steel.common.util.StringUtils;
+import com.cisdi.steel.module.job.AbstractExcelReadWriter;
 import com.cisdi.steel.module.job.dto.WriterExcelDTO;
 import com.cisdi.steel.module.job.util.date.DateQuery;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * 工艺参数
@@ -21,7 +29,7 @@ import java.util.List;
  * @version 1.0
  */
 @Component
-public class BF6gongyicanshuWriter extends GuankongzhibiaoWriter {
+public class BF6gongyicanshuWriter extends AbstractExcelReadWriter {
     @Override
     public Workbook excelExecute(WriterExcelDTO excelDTO) {
         Workbook workbook = this.getWorkbook(excelDTO.getTemplate().getTemplatePath());
@@ -52,6 +60,42 @@ public class BF6gongyicanshuWriter extends GuankongzhibiaoWriter {
             }
         }
         return workbook;
+    }
+
+    protected void setSheetValue(Sheet sheet, Integer rowNum, Integer columnNum, Object obj) {
+        Row row = sheet.getRow(rowNum);
+        if (Objects.isNull(row)) {
+            row = sheet.createRow(rowNum);
+        }
+        Cell cell = row.getCell(columnNum);
+        if (Objects.isNull(cell)) {
+            cell = row.createCell(columnNum);
+        }
+        PoiCustomUtil.setCellValue(cell, obj);
+    }
+
+    protected Double mapDataHandler(String url, String tagName, DateQuery dateQuery) {
+        Map<String, String> queryParam = getQueryParam(dateQuery);
+        queryParam.put("tagname", tagName);
+        String result = httpUtil.get(url, queryParam);
+        BigDecimal val = BigDecimal.ZERO;
+        if (StringUtils.isNotBlank(result)) {
+            JSONObject jsonObject = JSONObject.parseObject(result);
+            if (Objects.nonNull(jsonObject)) {
+                JSONObject data = jsonObject.getJSONObject("data");
+                if (Objects.nonNull(data)) {
+                    val = data.getBigDecimal("val");
+                }
+            }
+        }
+        return val.doubleValue();
+    }
+
+    @Override
+    protected Map<String, String> getQueryParam(DateQuery dateQuery) {
+        Map<String, String> result = new HashMap<>();
+        result.put("time", dateQuery.getRecordDate().getTime() + "");
+        return result;
     }
 
 }
