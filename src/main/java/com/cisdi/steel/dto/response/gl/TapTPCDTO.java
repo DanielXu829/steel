dcfbba12f2e039a099edf1193885ec9274632ea4
suntao <s6511@163.com>
2019-12-26 13:53:40 +0800

    增加tapTPC的dto

---
+++ src/main/java/com/cisdi/steel/dto/response/gl/TapTPCDTO.java
@@ -0,0 +1,11 @@
+package com.cisdi.steel.dto.response.gl;
+
+import com.cisdi.steel.dto.response.gl.res.TapTPC;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class TapTPCDTO {
+    private List<TapTPC> data;
+}
---
+++ src/main/java/com/cisdi/steel/dto/response/gl/res/TapTPC.java
@@ -0,0 +1,20 @@
+package com.cisdi.steel.dto.response.gl.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TapTPC {
+    private Date clockTime;
+    private String tapno;
+    private String tpcNo;
+    private BigDecimal grossWt;
+    private BigDecimal tareWt;
+    private BigDecimal netWt;
+}
