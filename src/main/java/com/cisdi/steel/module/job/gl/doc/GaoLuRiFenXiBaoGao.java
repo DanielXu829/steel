be37677e987f3128cb8773632d14f6192d1676a5
LongHui <blair.long@augmentum.com>
2020-06-05 15:57:02 +0800

    update schedule

--- src/main/java/com/cisdi/steel/module/job/gl/doc/GaoLuRiFenXiBaoGao.java
+++ src/main/java/com/cisdi/steel/module/job/gl/doc/GaoLuRiFenXiBaoGao.java
@@ -106,7 +106,7 @@ public class GaoLuRiFenXiBaoGao {
         longTimeList = new ArrayList<>();
     }
 
-    @Scheduled(cron = "0 0 23 * * ?")
+    @Scheduled(cron = "0 0 0/1 * * ?")
     public void mainTask() {
         initialData();
         initDateTime();
