35f07206739c605fe902afe44b2ddc7deeb17b61
易扬 <yiyangtry@163.com>
2020-07-24 13:18:20 +0800

    修复高炉日分析报告的和技术经济指标的产量理论累计不一致的问题

--- src/main/java/com/cisdi/steel/module/job/gl/doc/GaoLuRiFenXiBaoGao.java
+++ src/main/java/com/cisdi/steel/module/job/gl/doc/GaoLuRiFenXiBaoGao.java
@@ -984,7 +984,7 @@ public class GaoLuRiFenXiBaoGao extends AbstractExportWordJob {
         Double result;
         Double total = 0d;
         Double count = 0d;
-        List<Date> list = DateUtil.getAllDayBeginTimeInCurrentMonthBeforeDays(new Date(), -1);
+        List<Date> list = DateUtil.getAllDayBeginTimeInCurrentMonthBeforeDays(new Date(), 0);
         //3日燃料比累计=（1日燃料比*1日产量+2日燃料比*2日产量+3日燃料比*3日产量）/（1日产量+2日产量+3日产量）
         List<Double> tag1Object = getValuesByTag(data, tagName);
         //日产量
@@ -1011,7 +1011,7 @@ public class GaoLuRiFenXiBaoGao extends AbstractExportWordJob {
     private Double dealMonthTotal (JSONObject data, String tagName) {
         Double total = 0d;
         Double count = 0d;
-        List<Date> list = DateUtil.getAllDayBeginTimeInCurrentMonthBeforeDays(new Date(), -1);
+        List<Date> list = DateUtil.getAllDayBeginTimeInCurrentMonthBeforeDays(new Date(), 0);
         List<Double> tagObject = getValuesByTag(data, tagName);
         if(Objects.nonNull(tagObject) && Objects.nonNull(list)&& tagObject.size() > list.size() ) {
             for(int i = tagObject.size() - list.size() + 1; i < tagObject.size(); i ++) {
