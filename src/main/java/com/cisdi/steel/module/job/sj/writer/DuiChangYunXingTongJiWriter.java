04c5b30461ee239b1ba0259c393f10749114187c
易扬 <yiyangtry@163.com>
2020-08-21 16:52:54 +0800

    修复动态报表样式问题

--- src/main/java/com/cisdi/steel/module/job/sj/writer/DuiChangYunXingTongJiWriter.java
+++ src/main/java/com/cisdi/steel/module/job/sj/writer/DuiChangYunXingTongJiWriter.java
@@ -32,11 +32,11 @@ public class DuiChangYunXingTongJiWriter extends AbstractExcelReadWriter {
             log.error("在模板中获取version失败", e);
         }
 
-        try {
-            getMapHandler1(excelDTO, workbook, version);
-        } catch (Exception e) {
-            log.error("处理模板数据失败", e);
-        }
+        Date searchDate = this.getDateQueryBeforeOneDay(excelDTO).getRecordDate();
+        Sheet sheet1 = workbook.getSheetAt(0);
+        ExcelWriterUtil.replaceCurrentMonthInTitle(sheet1, 0, 0, searchDate);
+        ExcelWriterUtil.replaceCurrentDateInTitle(sheet1, "%当日数%", searchDate, DateUtil.ddFormat);
+        getMapHandler1(excelDTO, workbook, version);
 
         return workbook;
     }
@@ -49,50 +49,50 @@ public class DuiChangYunXingTongJiWriter extends AbstractExcelReadWriter {
      * @return
      */
     private void getMapHandler1(WriterExcelDTO excelDTO, Workbook workbook, String version) {
-        String sheetName = "_data";
-        Sheet sheet = workbook.getSheet(sheetName);
-        List<CellData> resultList = new ArrayList<>();
+        try {
+            String sheetName = "_data";
+            Sheet sheet = workbook.getSheet(sheetName);
+            List<CellData> resultList = new ArrayList<>();
 
-        Cell pushMatCountCell = PoiCustomUtil.getCellByValue(sheet, "PUSHMATCOUNT");
-        int pushMatCountColIndex = pushMatCountCell.getColumnIndex();
-        Cell pushMatTimeCell = PoiCustomUtil.getCellByValue(sheet, "PUSHMATTIME");
-        int pushMatTimeColIndex = pushMatTimeCell.getColumnIndex();
-        Cell pushMatCell = PoiCustomUtil.getCellByValue(sheet, "PUSHMAT");
-        int pushMatColIndex = pushMatCell.getColumnIndex();
-        Cell pullMatCountCell = PoiCustomUtil.getCellByValue(sheet, "PULLMATCOUNT");
-        int pullMatCountColIndex = pullMatCountCell.getColumnIndex();
-        Cell pullMatTimeCell = PoiCustomUtil.getCellByValue(sheet, "PULLMATTIME");
-        int pullMatTimeColIndex = pullMatTimeCell.getColumnIndex();
-        Cell pullMatCell = PoiCustomUtil.getCellByValue(sheet, "PULLMAT");
-        int pullMatColIndex = pullMatCell.getColumnIndex();
+            Cell pushMatCountCell = PoiCustomUtil.getCellByValue(sheet, "PUSHMATCOUNT");
+            int pushMatCountColIndex = pushMatCountCell.getColumnIndex();
+            Cell pushMatTimeCell = PoiCustomUtil.getCellByValue(sheet, "PUSHMATTIME");
+            int pushMatTimeColIndex = pushMatTimeCell.getColumnIndex();
+            Cell pushMatCell = PoiCustomUtil.getCellByValue(sheet, "PUSHMAT");
+            int pushMatColIndex = pushMatCell.getColumnIndex();
+            Cell pullMatCountCell = PoiCustomUtil.getCellByValue(sheet, "PULLMATCOUNT");
+            int pullMatCountColIndex = pullMatCountCell.getColumnIndex();
+            Cell pullMatTimeCell = PoiCustomUtil.getCellByValue(sheet, "PULLMATTIME");
+            int pullMatTimeColIndex = pullMatTimeCell.getColumnIndex();
+            Cell pullMatCell = PoiCustomUtil.getCellByValue(sheet, "PULLMAT");
+            int pullMatColIndex = pullMatCell.getColumnIndex();
 
-        // 当天早上运行，查询截至前一天23:59:59
-        Date searchDate = this.getDateQueryBeforeOneDay(excelDTO).getRecordDate();
-        String searchDateStr = DateUtil.getFormatDateTime(searchDate, "yyyy-MM-dd 22:00:00");
-        searchDate = DateUtil.strToDate(searchDateStr, "yyyy-MM-dd HH:mm:ss");
-        Integer[] workTeams = {1, 2, 3, 4};
+            // 当天早上运行，查询截至前一天23:59:59
+            Date searchDate = this.getDateQueryBeforeOneDay(excelDTO).getRecordDate();
+            String searchDateStr = DateUtil.getFormatDateTime(searchDate, "yyyy-MM-dd 22:00:00");
+            searchDate = DateUtil.strToDate(searchDateStr, "yyyy-MM-dd HH:mm:ss");
+            Integer[] workTeams = {1, 2, 3, 4};
 
-        for (Integer workTeam : workTeams) {
-            String result = getDuiChangeYunXingTongJi(searchDate.getTime(), workTeam, version);
-            if (StringUtils.isNotBlank(result)) {
-                YardRunInfoDTO yardRunInfoDTO = JSON.parseObject(result, YardRunInfoDTO.class);
-                if (yardRunInfoDTO == null || yardRunInfoDTO.getData() == null) {
-                    return;
+            for (Integer workTeam : workTeams) {
+                String result = getDuiChangeYunXingTongJi(searchDate.getTime(), workTeam, version);
+                if (StringUtils.isNotBlank(result)) {
+                    YardRunInfoDTO yardRunInfoDTO = JSON.parseObject(result, YardRunInfoDTO.class);
+                    if (yardRunInfoDTO == null || yardRunInfoDTO.getData() == null) {
+                        return;
+                    }
+                    ExcelWriterUtil.addCellData(resultList, workTeam, pushMatCountColIndex, yardRunInfoDTO.getData().getPushMatCount());
+                    ExcelWriterUtil.addCellData(resultList, workTeam, pushMatTimeColIndex, yardRunInfoDTO.getData().getPushMatTime());
+                    ExcelWriterUtil.addCellData(resultList, workTeam, pushMatColIndex, yardRunInfoDTO.getData().getPushMat());
+                    ExcelWriterUtil.addCellData(resultList, workTeam, pullMatCountColIndex, yardRunInfoDTO.getData().getPullMatCount());
+                    ExcelWriterUtil.addCellData(resultList, workTeam, pullMatTimeColIndex, yardRunInfoDTO.getData().getPullMatTime());
+                    ExcelWriterUtil.addCellData(resultList, workTeam, pullMatColIndex, yardRunInfoDTO.getData().getPullMat());
                 }
-                ExcelWriterUtil.addCellData(resultList, workTeam, pushMatCountColIndex, yardRunInfoDTO.getData().getPushMatCount());
-                ExcelWriterUtil.addCellData(resultList, workTeam, pushMatTimeColIndex, yardRunInfoDTO.getData().getPushMatTime());
-                ExcelWriterUtil.addCellData(resultList, workTeam, pushMatColIndex, yardRunInfoDTO.getData().getPushMat());
-                ExcelWriterUtil.addCellData(resultList, workTeam, pullMatCountColIndex, yardRunInfoDTO.getData().getPullMatCount());
-                ExcelWriterUtil.addCellData(resultList, workTeam, pullMatTimeColIndex, yardRunInfoDTO.getData().getPullMatTime());
-                ExcelWriterUtil.addCellData(resultList, workTeam, pullMatColIndex, yardRunInfoDTO.getData().getPullMat());
             }
-        }
 
-        ExcelWriterUtil.setCellValue(sheet, resultList);
-        // 设置报表标题
-        Sheet sheet1 = workbook.getSheetAt(0);
-        ExcelWriterUtil.replaceCurrentMonthInTitle(sheet1, 0, 0, searchDate);
-        ExcelWriterUtil.replaceCurrentDateInTitle(sheet1, "%当日数%", searchDate, DateUtil.ddFormat);
+            ExcelWriterUtil.setCellValue(sheet, resultList);
+        } catch (Exception e) {
+            log.error("处理模板数据失败", e);
+        }
     }
 
     /**
